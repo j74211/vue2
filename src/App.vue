4278d65ef9cd3286e36573a4a533684d1e2402ca
j74211 <3075571927@qq.com>
2023-08-15 17:29:34 +0800

    修改查看插件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" class="iogo" src="https://img2.baidu.com/it/u=267632856,3740526320&fm=253&fmt=auto&app=120&f=JPEG?w=1280&h=800">
-    <HelloWorld msg="新时代没有能够承载我的船，艾斯！！！"/>
+    <HelloWorld msg="新时代没有能够承载我的船，艾斯！！！再一次修改"/>
   </div>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
